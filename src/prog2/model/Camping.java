f96202599f338d0c43242577b361fa7b2bcce002
“Biel007O010” <bielflaquegrive@gmail.com>
2026-02-24 12:03:08 +0100

    metodes afegits  la clase camping

--- src/prog2/model/Camping.java
+++ src/prog2/model/Camping.java
@@ -21,13 +21,56 @@ public class Camping implements InCamping{
         this.reserves = new ArrayList<>();
     }
 
+    private String buscarClient(String dni){
+
+    }
+
+    public static InAllotjament.Temp getTemporada(LocalDate data){
+
+    }
+
+    public String buscarAllotjament(String identificador) {
+
+    }
+
     @Override
     public String getNom() {
+
         return nom;
     }
 
+    @Override
+    public LlistaReserves getLlistaReserves() {
+        return null;
+    }
 
-    public String buscarAllotjament(String identificador) {
+    @Override
+    public ArrayList<Allotjament> getLlistaAllotjaments() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Client> getLlistaClients() {
+        return null;
+    }
+
+    @Override
+    public int getNumAllotjaments() {
+        return 0;
+    }
+
+    @Override
+    public int getNumReserves() {
+        return 0;
+    }
+
+    @Override
+    public int getNumClients() {
+        return 0;
+    }
+
+    @Override
+    public void afegirClient(String nom_, String dni_) {
 
     }
 
@@ -36,12 +79,41 @@ public class Camping implements InCamping{
 
     }
 
-    private String buscarClient(String dni){
+    @Override
+    public int calculAllotjamentsOperatius() {
+        return 0;
+    }
+
+    @Override
+    public Allotjament getAllotjamentEstadaMesCurta(InAllotjament.Temp temp) {
+        return null;
+    }
+
+
+    @Override
+    public void afegirParcela(String nom_, String idAllotjament_, float metres, boolean connexioElectrica){
 
     }
 
-    public static InAllotjament.Temp getTemporada(LocalDate data){
+    @Override
+    public void afegirBungalow(String nom_, String idAllotjament_, String mida, int habitacions, int placesPersones, int placesParquing, boolean terrassa, boolean tv, boolean aireFred){
 
     }
+
+    @Override
+    public void afegirBungalowPremium(String nom_, String idAllotjament_, String mida, int habitacions, int placesPersones, int placesParquing, boolean terrassa, boolean tv, boolean aireFred, boolean serveisExtra, String codiWifi){
+
+    }
+
+    @Override
+    public void afegirGlamping(String nom_, String idAllotjament_, String mida, int habitacions, int placesPersones, String material, boolean casaMascota) {
+
+    }
+
+    @Override
+    public void afegirMobilHome(String nom_, String idAllotjament_, String mida, int habitacions, int placesPersones, boolean terrassaBarbacoa) {
+
+    }
+
 
 }
